c1904d03107ee989e8a79ad4b2386ffa8e18b252
bajurski <bajurski@hotmail.com>
2022-12-11 17:10:15 +0100

    Formatka i chwytanie danych zrobione.

--- values.php
+++ values.php
@@ -1,14 +1,29 @@
 <?php
 if(isset($_POST['imie'])) {
     $imie = $_POST['imie'];
-    echo 'Imie :'.$imie;
+    echo 'Imie :'.$imie."<br>\n";
 }
 if(isset($_POST['nazwisko'])) {
     $imie = $_POST['nazwisko'];
-    echo 'Imie :'.$imie;
+    echo 'Imie :'.$imie."<br>\n";
 }
 if(isset($_POST['radio1'])) {
-    $imie = $_POST[''];
-    echo 'Imie :'.$imie;
+    $gender = $_POST['radio1'];
+    echo 'Płeć :'.$gender."<br>\n";
 }
+if(isset($_POST['password'])) {
+    $password = $_POST['password'];
+    echo 'Hasło :'.$password."<br>\n";
+}
+if(isset($_POST['nazwa'])) {
+    $textField = $_POST['nazwa'];
+    echo 'Tekst :'.$textField."<br>\n";
+}
+if(isset($_POST['checkbox'])) {
+    $password = $_POST['checkbox'];
+    echo 'Zgadzam się z warunkami umowy'."<br>\n";
+} else {
+    echo 'Mam was wszystkich w DUPIE !!!'."<br>\n";
+}
+
 
