7ed7776df4a1ed0328c3b7f9583751f01db9eaba
bajurski <bajurski@hotmail.com>
2022-12-11 15:38:30 +0100

    sdgasgdf

--- values.php
+++ values.php
@@ -1,1 +1,14 @@
 <?php
+if(isset($_POST['imie'])) {
+    $imie = $_POST['imie'];
+    echo 'Imie :'.$imie;
+}
+if(isset($_POST['nazwisko'])) {
+    $imie = $_POST['nazwisko'];
+    echo 'Imie :'.$imie;
+}
+if(isset($_POST['radio1'])) {
+    $imie = $_POST[''];
+    echo 'Imie :'.$imie;
+}
+
